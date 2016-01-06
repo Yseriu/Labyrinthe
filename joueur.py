6632b2d1ea68fb3a19e809367304a6fe9d117ddf
Baptiste Barbieri <barbieri@info27-13.iut45.univ-orleans.fr>
2016-01-06 17:27:25 +0100

    Fonctions sur les joueurs (1)

--- joueur.py
+++ joueur.py
@@ -3,12 +3,23 @@ import random
 # permet de créer entre deux et quatre joueurs et leur distribue de manière équitable
 # les trésors compris entre 1 et nbTresor avec au plus nbMaxTresor chacun
 # si nbMaxTresor vaut 0, la fonction distribue le maximum de trésors possible
+
+# Le dictionnaire de joueurs contient :
+#  * owner : la liste des propriétaires des trésors - n° de trésor en indice, n° de proprio en valeur
+#  * found : la liste des trésor : valeur -> boolean trouvé ou non
+
 def Joueurs( nbJoueurs=2, nbTresors=24, nbTresorMax=0):
-    pass
+	nbTr = min(nbTresors, nbTresorMax*nbJoueurs) if nbTresorMax != 0 else nbTresors
+    return {'NbJoueurs' : nbJoueurs,
+			'Owner' : [0] * nbTr,
+			'Found' : [False] * nbTr}
 
 # attribue effectivement les trésors de manière aléatoire
 def initTresor(joueurs):
-    pass
+    tresorParJoueur = len(joueurs['Owner'])/joueurs['NbJoueurs']
+    tres = [0]*joueurs['NbJoueurs']
+    for i in range(len(joueurs['Owner'])):
+		pass
 
 # retourne le numéro du prochain trésor à trouver pour la joueur numJoueur
 # None s'il n'y a pas de prochain trésor
